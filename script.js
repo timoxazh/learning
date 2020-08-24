275135e406f7b897772bdf3e13ddcf375a6d0e31
Timofey Zhurin <timoxazh1@gmail.com>
2020-08-25 04:52:21 +0700

    final with questions

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger, .header__menu').toggleClass('menu-active')
+    })
+    
+    $('.menu__close').click(function(event) {
+        $('.header__burger, .header__menu').removeClass('menu-active')
+    })
+})
